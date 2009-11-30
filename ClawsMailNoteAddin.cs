09062a099313413612c9b6e753996d85a307f813
Holger Berndt <berndth@gmx.de>
2009-11-30 01:26:28 +0100

    basic handling of mail drops

--- ClawsMailNoteAddin.cs
+++ ClawsMailNoteAddin.cs
@@ -1,8 +1,13 @@
 // Heavily influenced by the Evolution addin
-
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
 using Mono.Unix;
-using Gtk;
+using Mono.Unix.Native;
+
 using Tomboy;
 
 namespace Tomboy.ClawsMail
@@ -15,7 +20,7 @@ namespace Tomboy.ClawsMail
         {
 			get {
 				if(mail_icon == null)
-					mail_icon = GuiUtils.GetIcon (System.Reflection.Assembly.GetExecutingAssembly(), "mail", 16);
+					mail_icon = GuiUtils.GetIcon(System.Reflection.Assembly.GetExecutingAssembly(), "mail", 16);
 				return mail_icon;
 			}
         }
@@ -33,24 +38,23 @@ namespace Tomboy.ClawsMail
 			CanActivate = true;
 
 			Image = MailIcon;
-		}
+		}     
 
-		public string EmailUri
+		protected override bool OnActivate (NoteEditor editor,
+		                                    Gtk.TextIter start,
+		                                    Gtk.TextIter end)
 		{
-			get {
-				return (string) Attributes ["uri"];
-			}
-			set {
-				Attributes ["uri"] = value;
-			}
-		}        
-        
+            return true;
+        }        
     }
     
     public class ClawsMailNoteAddin : NoteAddin
     {
+        List<string> subject_list;
+        
 		static ClawsMailNoteAddin()
 		{
+            GMime.Global.Init();
 		}
         
         public override void Initialize()
@@ -68,14 +72,101 @@ namespace Tomboy.ClawsMail
             Window.Editor.DragDataReceived += OnDragDataReceived;
 		}
 
+		[DllImport("libgobject-2.0.so.0")]
+		static extern void g_signal_stop_emission_by_name (IntPtr raw, string name);
+
 		[GLib.ConnectBefore]
 		void OnDragDataReceived(object sender, Gtk.DragDataReceivedArgs args)
 		{
+            bool stop_emission = false;
+            
 			if(args.SelectionData.Length < 0)
 				return;
             
-            Logger.Info("ClawsMail: OnDragDataReceived");
+            if(args.Info == 1) {
+                foreach (Gdk.Atom atom in args.Context.Targets) {
+                    if(atom.Name == "text/uri-list") {
+                        DropEmailUriList(args);
+                        stop_emission = true;
+                        InsertMailLinks(args.X, args.Y, subject_list);
+                    }
+                }
+            }
+            
+			if(stop_emission) 
+				g_signal_stop_emission_by_name(Window.Editor.Handle, "drag-data-received");
 		}
+        
+        void DropEmailUriList(Gtk.DragDataReceivedArgs args)
+        {
+			string uri_string = Encoding.UTF8.GetString(args.SelectionData.Data);
+
+			subject_list = new List<string>();
+
+			UriList uri_list = new UriList(uri_string);
+
+			foreach(Uri uri in uri_list) {
+                Logger.Info("Claws Mail: Dropped URI: {0}", uri.LocalPath);
+                int mail_fd = Syscall.open(uri.LocalPath, OpenFlags.O_RDONLY);
+				if(mail_fd == -1)
+					continue;
+                    
+                GMime.Stream stream = new GMime.StreamFs(mail_fd);
+                GMime.Parser parser = new GMime.Parser(stream);
+
+                parser.ScanFrom = false;
+				while(!parser.Eos()) {
+					GMime.Message message = parser.ConstructMessage();
+					if(message == null)
+						break;
+					
+					Logger.Info("Claws Mail: Message Subject: {0}", message.Subject);
+					subject_list.Add(message.Subject);
+					message.Dispose();
+				};
+
+				parser.Dispose();
+				stream.Close();
+				stream.Dispose();
+            }
+        }
+        
+		void InsertMailLinks (int x, int y, List<string> subject_list)
+		{
+			// Place the cursor in the position where the uri was
+			// dropped, adjusting x,y by the TextView's VisibleRect.
+			Gdk.Rectangle rect = Window.Editor.VisibleRect;
+			x = x + rect.X;
+			y = y + rect.Y;
+			Gtk.TextIter cursor = Window.Editor.GetIterAtLocation (x, y);
+			Buffer.PlaceCursor (cursor);
+
+            bool more_than_one = false;
+			foreach (string subject in subject_list) {
+                int start_offset;
                 
-    }
+                if(more_than_one) {
+                    cursor = Buffer.GetIterAtMark (Buffer.InsertMark);
+
+                    if(cursor.LineOffset == 0)
+                        Buffer.Insert(ref cursor, "\n");
+                    else
+                        Buffer.Insert(ref cursor, ", ");
+				}
+
+                
+                EmailLink link_tag;
+                link_tag = (EmailLink) Note.TagTable.CreateDynamicTag("link:cm-mail");
+                cursor = Buffer.GetIterAtMark(Buffer.InsertMark);
+                start_offset = cursor.Offset;
+                Buffer.Insert(ref cursor, subject);
+				Gtk.TextIter start = Buffer.GetIterAtOffset (start_offset);
+				Gtk.TextIter end = Buffer.GetIterAtMark (Buffer.InsertMark);
+				Buffer.ApplyTag (link_tag, start, end);                
+                
+                more_than_one = true;
+            }
+        }
+
+    } // ClawsMailNoteAddin
 }
--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	gmcs -debug -out:ClawsMail.dll -target:library -pkg:tomboy-addins -r:Mono.Posix ClawsMailNoteAddin.cs -resource:ClawsMail.addin.xml
+	gmcs -debug -out:ClawsMail.dll -target:library -pkg:tomboy-addins -r:Mono.Posix ClawsMailNoteAddin.cs `pkg-config --libs gmime-sharp-2.2` -resource:ClawsMail.addin.xml -resource:mail.png
 
 install:
 	cp ClawsMail.dll $(HOME)/.config/tomboy/addins
